fe296cbbbf72428d8563414d1d8e5c6d14872a86
Huy Le <ing.huyle@gmail.com>
2024-09-26 20:08:04 +0200

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import "./styles/App.scss";
+import $ from "jquery";
+import { useState, useEffect } from "react";
+
+const colors = [
+  "#fb6964",
+  "#bdbb99",
+  "#16a085",
+  "#73a857",
+  "#2c3e50",
+  "#f39c12",
+  "#472e32",
+  "#9b59b6",
+  "#e74c3c"
+];
+
+export default function App() {
+  const [index, setIndex] = useState(Math.floor(Math.random() * colors.length));
+  const [content, setContent] = useState("Everything exists.");
+  const [author, setAuthor] = useState("Huy");
+
+  const handleClick = () => {
+    let i = Math.floor(Math.random() * colors.length);
+    while (i === index) {
+      i = Math.floor(Math.random() * colors.length);
+    }
+
+    setIndex(i);
+  }
+
+  const fetchData = async () =>{
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      setContent(data.content);
+      setAuthor(data.author);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    $("#text").addClass("fade");
+    $("#author").addClass("fade");
+
+    const timerFetch = setTimeout(() => {
+      fetchData();
+    }, 750);
+
+    const timerFade = setTimeout(() => {
+      $("#text").removeClass("fade");
+      $("#author").removeClass("fade");
+    }, 1500);
+
+    return () => {
+      clearTimeout(timerFetch);
+      clearTimeout(timerFade);
+    };
+  }, [index]);
+
+  let color = colors[index];
+  $("body").css("background-color", color);
+
+  return (
+    <div id="quote-box">
+      <div id="text" style={{ color: color }}>{content}</div>
+      <div id="author" style={{ color: color }}>{author}</div>
+      <div className="btn-row">
+        <button className="btn" style={{ backgroundColor: color }}>
+          <a className="btn-icon" href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${content}`} target="_blank">
+            <svg viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" /></svg>
+          </a>
+        </button>
+        <button className="btn btn-text" style={{ backgroundColor: color }} onClick={handleClick}>New quote</button>
+      </div>
+    </div>
+  );
+}
