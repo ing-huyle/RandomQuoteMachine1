7698e9f30a475273ce01cf4b552aa2b1cb7403b0
Huy Le <ing.huyle@gmail.com>
2024-10-07 17:12:49 +0200

    better api source

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const colors = [
 
 export default function App() {
   const [index, setIndex] = useState(Math.floor(Math.random() * colors.length));
-  const [content, setContent] = useState("Everything exists.");
+  const [quote, setQuote] = useState("Everything exists.");
   const [author, setAuthor] = useState("Huy");
 
   const handleClick = () => {
@@ -30,10 +30,14 @@ export default function App() {
 
   const fetchData = async () =>{
     try {
-      const response = await fetch("https://api.quotable.io/random");
+      const response = await fetch("https://api.api-ninjas.com/v1/quotes", {
+        method: 'GET',
+        headers: { 'X-Api-Key': `uuG8lwcXmp4I/mR0J2Yk6Q==0UX3cz7EFt25Afmj` }
+      });
       const data = await response.json();
-      setContent(data.content);
-      setAuthor(data.author);
+      console.log(data);
+      setQuote(data[0].quote);
+      setAuthor(data[0].author);
     } catch (error) {
       console.log(error);
     }
@@ -63,11 +67,11 @@ export default function App() {
 
   return (
     <div id="quote-box">
-      <div id="text" style={{ color: color }}>{content}</div>
+      <div id="text" style={{ color: color }}>{quote}</div>
       <div id="author" style={{ color: color }}>{author}</div>
       <div className="btn-row">
         <button className="btn" style={{ backgroundColor: color }}>
-          <a className="btn-icon" href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${content}`} target="_blank">
+          <a className="btn-icon" href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote}`} target="_blank">
             <svg viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" /></svg>
           </a>
         </button>
